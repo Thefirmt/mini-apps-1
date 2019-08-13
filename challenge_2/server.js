1f21e17b36788067b1fe399b09598a882338df7a
Alex Essel <thefirmt@gmail.com>
2019-08-13 12:23:12 -0700

    Created basic server. Added simple .get and .post functionality

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static(__dirname + 'client'));
+app.use(express.urlencoded());
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client' + '/index.html')
+})
+
+app.post('/', (req, res) =>{
+    console.log(JSON.stringify(req.body.data));
+    res.sendFile(__dirname + '/client' + '/index.html')
+})
+
+app.listen(port, () => {console.log(`Server is running on port ${port}!`)});
