20efc3306be3a7f3e1cf6e7c3fea2a2c55e116e1
Alex Essel <thefirmt@gmail.com>
2019-08-15 12:49:56 -0700

    Setting up basic components and environment

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Square from './Squares.jsx'
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            something: "here"
+        }
+    }
+
+    firstMethod() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Square />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
