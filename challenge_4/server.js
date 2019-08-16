79f04ded8bb4b01aa9c9bb8341c02a77565fdc9e
Alex Essel <thefirmt@gmail.com>
2019-08-15 17:27:02 -0700

    Application Served through Express and completed.

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -3,4 +3,10 @@ const app = express();
 const port = 3000;
 
 
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public' + '/index.html')
+})
+
+
 app.listen(port, () => console.log(`listening on port ${port}`));
